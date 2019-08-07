7754afd780ffb81973469c5de6ac90e20be6db33
Starke427 <jstarke@superuser.life>
2019-08-07 16:21:11 -0700

    Create enable_secure_policy.ps1

---
+++ enable_secure_policy.ps1
@@ -0,0 +1,18 @@
+# Download from url and write to local file in C:\temp, similar to curl.
+# When executed, will download and run the audit and password policy scripts.
+# Automatically keeps up to date with latest versioning. Commenting out powershell.exe will allow you to edit scripts prior to execution.
+#
+# Author: Jeff Starke
+
+$url1 = "https://raw.githubusercontent.com/Starke427/Windows-Security-Policy/master/configure_audit_policy.ps1"
+$file1 = "$env:temp\configure_audit_policy.ps1"
+
+$url2 = "https://raw.githubusercontent.com/Starke427/Windows-Security-Policy/master/configure_password_policy.ps1"
+$file2 = "$env:temp\configure_password_policy.ps1"
+
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url1, $file1)
+(New-Object -TypeName System.Net.WebClient).DownloadFile($url2, $file2)
+
+
+powershell.exe -ExecutionPolicy ByPass -File $file1
+powershell.exe -ExecutionPolicy ByPass -File $file2
